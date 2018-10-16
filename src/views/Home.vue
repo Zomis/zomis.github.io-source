9f5050f035460533d2027cb05d809b27fb06b2fd
Simon Forsberg <zomis2k@hotmail.com>
2018-10-16 19:12:00 +0200

    Add LinkedIn and improve flairs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,17 +13,52 @@
           </footer>
         </blockquote>
 -->
-        <div>
-          <a href="https://stackoverflow.com/users/1310566/simon-forsberg">
-            <img src="https://stackoverflow.com/users/flair/1310566.png" width="208" height="58" alt="profile for Simon Forsberg at Stack Overflow, Q&amp;A for professional and enthusiast programmers" title="profile for Simon Forsberg at Stack Overflow, Q&amp;A for professional and enthusiast programmers">
-          </a>
-          <a href="https://codereview.stackexchange.com/users/31562/simon-forsberg">
-            <img src="https://codereview.stackexchange.com/users/flair/31562.png" width="208" height="58" alt="profile for Simon Forsberg at Code Review Stack Exchange, Q&amp;A for peer programmer code reviews" title="profile for Simon Forsberg at Code Review Stack Exchange, Q&amp;A for peer programmer code reviews">
-          </a>
-          <a href="https://stackexchange.com/users/1376679">
-            <img src="https://stackexchange.com/users/flair/1376679.png" width="208" height="58" alt="profile for Simon Forsberg on Stack Exchange, a network of free, community-driven Q&amp;A sites" title="profile for Simon Forsberg on Stack Exchange, a network of free, community-driven Q&amp;A sites">
-          </a>
-        </div>
+
+        <v-card>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">Simon Forsberg</h3>
+              <div>Who he is and what he's done</div>
+            </div>
+          </v-card-title>
+          <v-card-text>
+            If you want to connect with me on LinkedIn, please also add a message about how you found me. Did you listen to one of my talks perhaps?
+          </v-card-text>
+          <v-card-actions>
+            <v-btn href="https://www.linkedin.com/in/simonforsberg/" flat color="blue">Connect</v-btn>
+          </v-card-actions>
+        </v-card>
+
+        <v-card class="flairs">
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">Stack Exchange Network</h3>
+              <div>Stack Overflow and Code Review Stack Exchange</div>
+            </div>
+          </v-card-title>
+          <div class="flair">
+            <a href="https://stackoverflow.com/users/1310566/simon-forsberg">
+              <img src="https://stackoverflow.com/users/flair/1310566.png" width="208" height="58" alt="profile for Simon Forsberg at Stack Overflow, Q&amp;A for professional and enthusiast programmers" title="profile for Simon Forsberg at Stack Overflow, Q&amp;A for professional and enthusiast programmers">
+            </a>
+          </div>
+          <div class="flair">
+            <a href="https://codereview.stackexchange.com/users/31562/simon-forsberg">
+              <img src="https://codereview.stackexchange.com/users/flair/31562.png" width="208" height="58" alt="profile for Simon Forsberg at Code Review Stack Exchange, Q&amp;A for peer programmer code reviews" title="profile for Simon Forsberg at Code Review Stack Exchange, Q&amp;A for peer programmer code reviews">
+            </a>
+          </div>
+          <!--
+          <div class="flair">
+            <a href="https://stackexchange.com/users/1376679">
+              <img src="https://stackexchange.com/users/flair/1376679.png" width="208" height="58" alt="profile for Simon Forsberg on Stack Exchange, a network of free, community-driven Q&amp;A sites" title="profile for Simon Forsberg on Stack Exchange, a network of free, community-driven Q&amp;A sites">
+            </a>
+          </div>
+        -->
+          <v-card-actions>
+            <v-btn href="https://stackoverflow.com/users/1310566/simon-forsberg" flat color="orange">Stack Overflow</v-btn>
+            <v-btn href="https://codereview.stackexchange.com/users/31562/simon-forsberg" flat color="blue">Code Review</v-btn>
+          </v-card-actions>
+        </v-card>
+
 
 
         <v-card>
@@ -67,8 +102,16 @@
   </v-container>
 </template>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.v-card {
+  width: 500px;
+  margin-bottom: 42px;
+}
+
+.flair {
+  margin-left: 15px;
+}
+
 .simon {
   height: 300px;
 }
